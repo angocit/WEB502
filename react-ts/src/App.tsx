1cbf7e8fe443214e246eda4ec8e794cc2c9d840a
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-03-13 18:00:26 +0700

    Tạo router

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -5,15 +5,19 @@ import './App.css'
 import Header from './components/header'
 import Footer from './components/footer'
 import Content from './components/content'
+import { Route, Routes } from 'react-router-dom'
+import Homepage from './components/homepage'
+import ProductList from './components/productlist'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <>
-     <Header title = "Đây là title gửi từ App"/>
-     <Content/>
-     <Footer/>
+     <Routes>
+          <Route path='/' Component={Homepage}/>
+          <Route path='/products' Component={ProductList}/>
+     </Routes>
    </>
   )
 }
---
+++ react-ts/src/components/productlist.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './header'
+import Footer from './footer'
+
+type Props = {}
+
+const ProductList = (props: Props) => {
+  return (
+    <>
+    <Header title='Đây là trang danh sách sản phẩm'/>
+    <Footer/>
+   </>
+  )
+}
+
+export default ProductList
