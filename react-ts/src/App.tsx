6d2d888b330a43a694851c3f7370dfd4ce9aa397
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-03-25 18:13:22 +0700

    B10

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -2,13 +2,27 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/home'
 import Product from './components/product'
+import { useEffect, useState } from 'react'
+import { IProduct } from './types/product'
 
 function App() {
+  const [products,setProduct]=useState<IProduct[]>([])
+  const getProduct = ()=>{
+    fetch(`http://localhost:3000/products`)
+    .then((response:any)=>response.json())
+    .then((product:IProduct[])=>{
+      setProduct(product)
+    })
+    .catch((error:any)=>{
+        console.log(`can not load product`);          
+    }) 
+  }
+  useEffect(getProduct,[])
   return (
    <>
        <Routes>
           <Route path='/' Component={Home}/>
-          <Route path='/products' Component={Product}/>
+          <Route path='/products' element={<Product products={products} setProduct = {setProduct}/>}/>
        </Routes>
    </>
   )
--- react-ts/src/components/product.tsx
+++ react-ts/src/components/product.tsx
@@ -1,9 +1,27 @@
 import React, { useState } from 'react'
 import Header from './header'
 import Footer from './footer'
+import { IProduct } from '../types/product'
+import Joi from 'joi'
 
-type Props = {}
-
+type Props = {
+    products:IProduct[],
+    setProduct:(data:IProduct[])=>void
+}
+const validateObj = Joi.object({
+    name: Joi.string().required().empty().trim().messages({
+        "any:required": "Tên bắt buộc phải nhập",
+        "string.empty":"Tên không được để trống"
+    }),
+    image: Joi.string().required().empty().trim().messages({
+        "any:required": "Ảnh bắt buộc phải nhập",
+        "string.empty":"Ảnh không được để trống"
+    }),
+    price: Joi.number().required().empty().min(1000).messages({
+        "any:required": "Tên bắt buộc phải nhập",
+        "number.min":"Giá không nhỏ hơn 1000"
+    })
+})
 const Product = (props: Props) => {
     const [name,setName]=useState<string>('')
     const [image,setImage]=useState<string>('')
@@ -11,18 +29,27 @@ const Product = (props: Props) => {
     const [message,setMessage]=useState<string>('')
 const handleSubmit = (e:any)=>{
     e.preventDefault()
+    console.log(name,image,price);
+    const {error} = validateObj.validate({name,image,price})
+    if (error){
+        setMessage(error.message)
+    }
+    else {
     const option = {
         method:'POST',
         body: JSON.stringify({name,image,price})
     }
     fetch('http://localhost:3000/products',option)
     .then(response=>response.json())
-    .then(data=>{
-        setMessage(`Thêm thành công`);        
+    .then((data:IProduct)=>{
+        setMessage(`Thêm thành công`); 
+        const newproducts = [...props.products,data]   
+        props.setProduct(newproducts)   
     })
-    .catch(err =>{
-        setMessage(`Thêm không thành công`);
+    .catch((err:any) =>{
+        setMessage(`Thêm không thành công ${err.message}`);
     })
+    }
 }
   return (
     <>
@@ -35,6 +62,33 @@ const handleSubmit = (e:any)=>{
             <input onChange={(e:any)=>{setPrice(e.target.value)}} type='number' placeholder='Giá tiền'/>
             <button type='submit'>Đăng sản phẩm</button>
         </form>
+        <h3>Danh sách sản phẩm</h3>
+        <table>
+            <thead>
+                <tr>
+                    <th>STT</th>
+                    <th>Ảnh</th>
+                    <th>Tên</th>
+                    <th>Giá tiền</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    props.products.map((product:IProduct,index:number)=>{
+                        return (
+                            <tr>
+                                <td>{index+1}</td>
+                                <td>{product.image}</td>
+                                <td>{product.name}</td>
+                                <td>{product.price}</td>
+                                <td><button>Sửa</button><button>Xóa</button></td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
         <Footer/>
     </>
   )
