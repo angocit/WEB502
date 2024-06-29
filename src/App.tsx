fb6a46180d03a1913836bb90bdafd4e160feaa8c
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-06-29 15:20:12 +0700

    B3

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+interface ITodo{
+  id:number,
+  title:string,
+  completed:boolean
+}
 function App() {
-  const [count, setCount] = useState(0)
-  const tanggiatri = ()=>{
-    const newcount = count+1
-    setCount(newcount)
-  }
+  const [count, setCount] = useState<number>(0)
+  // const todo:ITodo[] = []
+  const todos:Array<ITodo> = [
+    {id:1, title:"Todo 1",completed:false},
+    {id:2, title:"Todo 2",completed:false},
+    {id:3, title:"Todo 3",completed:false}
+  ]
   return (
     <>
-      <div className="card">
-        count is {count}
-        <button onClick={() => tanggiatri()}>
-          Tăng
-        </button>
-        <button>
-          Giảm
-        </button>
-      </div>
+      <h1>Danh sách công việc</h1>
+      <ul>
+        {todos.map(todo=>
+         <li>{todo.title}</li>
+        )}
+      </ul>
     </>
   )
 }
