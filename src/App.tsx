a593332c27f0ba81bf4a1912d41763c7af754524
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-06-29 16:01:21 +0700

    B3

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,27 @@ interface ITodo{
   completed:boolean
 }
 function App() {
-  const [count, setCount] = useState<number>(0)
-  // const todo:ITodo[] = []
-  const todos:Array<ITodo> = [
+  // const [count, setCount] = useState<number>(0)
+  const [todos,setTodos] = useState<ITodo[]>([
     {id:1, title:"Todo 1",completed:false},
     {id:2, title:"Todo 2",completed:false},
     {id:3, title:"Todo 3",completed:false}
-  ]
+  ])
+  // const todo:ITodo[] = []
+  // const atodos:Array<ITodo> = [
+  //   {id:1, title:"Todo 1",completed:false},
+  //   {id:2, title:"Todo 2",completed:false},
+  //   {id:3, title:"Todo 3",completed:false}
+  // ]
+  const AddTodo = ():void=>{
+    const todo:ITodo = {id:4,title:"Todo 4",completed:false}
+    const newtodos = [...todos,todo]
+    setTodos(newtodos)
+  }
   return (
     <>
       <h1>Danh sách công việc</h1>
+      <button onClick={()=>AddTodo()}>Thêm CV</button>
       <ul>
         {todos.map(todo=>
          <li>{todo.title}</li>
