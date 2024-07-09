13ee1ebef0ecf5c720cb016b7bf5916254e6d386
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-07-09 15:02:54 +0700

    B4

--- src/App.tsx
+++ src/App.tsx
@@ -14,24 +14,26 @@ function App() {
     {id:2, title:"Todo 2",completed:false},
     {id:3, title:"Todo 3",completed:false}
   ])
-  // const todo:ITodo[] = []
-  // const atodos:Array<ITodo> = [
-  //   {id:1, title:"Todo 1",completed:false},
-  //   {id:2, title:"Todo 2",completed:false},
-  //   {id:3, title:"Todo 3",completed:false}
-  // ]
+  const [value,setValue] = useState<string>('')
   const AddTodo = ():void=>{
-    const todo:ITodo = {id:4,title:"Todo 4",completed:false}
+    const todo:ITodo = {id:todos.length+1,title:value,completed:false}
     const newtodos = [...todos,todo]
     setTodos(newtodos)
+  }
+  const onDelete = (id:number)=>{
+    if(confirm("Bạn chắc chứ?")){
+    const newtodos = todos.filter(todo=>todo.id!==id)
+    setTodos(newtodos)
+    }
   }
   return (
     <>
       <h1>Danh sách công việc</h1>
+      <input onChange={(e)=>setValue(e.target.value)} type='text' placeholder='Nhập CV vào đây'/>
       <button onClick={()=>AddTodo()}>Thêm CV</button>
       <ul>
         {todos.map(todo=>
-         <li>{todo.title}</li>
+         <li>{todo.title} <button onClick={()=>onDelete(todo.id)}>Xóa</button></li>
         )}
       </ul>
     </>
