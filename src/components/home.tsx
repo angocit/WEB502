c328bbafc141b1d12822c9e43403df3fcc711613
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-07-11 16:11:36 +0700

    B5

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ITodo } from '../App'
 
 type Props = {
@@ -6,8 +6,18 @@ type Props = {
 }
 
 const Home = ({todos}: Props) => {
+    const [count,setCount] = useState<number>(0)
+    const [count2,setCount2] = useState<number>(5)
+    useEffect(()=>{
+        console.log(`Render ${count}`);
+        
+    },[])
   return (
     <>
+    Số 1: {count} <br></br>
+    Số 2: {count2} <br></br>
+    <button onClick={()=>setCount(count+1)}>Tăng</button>
+    <button onClick={()=>setCount2(count2+1)}>Tăng Count2</button>
       <h1>Danh sách công việc</h1>
       <ul>
         {todos.map(todo=>
