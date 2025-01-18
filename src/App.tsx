3b1f04cc843537b06f9cf215a51e397c6b74d111
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-01-18 11:20:07 +0700

    b6

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,16 @@ import { useRoutes } from 'react-router-dom'
 import Home from './components/home'
 import AddProduct from './components/addproduct'
 import EditProduct from './components/editproduct'
+import OrderAdd from './components/orderadd'
+import OrderList from './components/orderlist'
 function App() {
     // Khai báo routes
     const routes = useRoutes([
       {path:'/',element:<Home/>},
       {path:'/product-add',element:<AddProduct/>},
-      {path:'/product-edit/:id',element:<EditProduct/>}
+      {path:'/product-edit/:id',element:<EditProduct/>},
+      {path:'/order-add',element:<OrderAdd/>},
+      {path:'/order-list',element:<OrderList/>}
     ])
     return routes
 }
